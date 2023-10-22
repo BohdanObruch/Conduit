21c11631c124b1c28932ce32830b943c5b9d3a65
BohdanObruch <bodan1992@gmail.com>
2023-10-22 06:35:13 +0300

    fixed fake data

--- demo_apps_project_tests/data/fake_data.py
+++ demo_apps_project_tests/data/fake_data.py
@@ -5,19 +5,13 @@ fake = Faker()
 
 def generate_random_user_data():
     user_data = {
-        "username": fake.user_name(),
+        "username": f'{random.randint(1, 100)}{fake.user_name()}',
         "email": f'{random.randint(1, 100)}{fake.email()}',
         "password": fake.password(),
         "picture": fake.image_url(),
         "bio": fake.text()
     }
     return user_data
-
-
-# user_data = generate_random_user_data()
-# username = user_data["username"]
-# email = user_data["email"]
-# password = user_data["password"]
 
 
 def generate_random_article():
@@ -32,10 +26,3 @@ def generate_random_article():
     for _ in range(num_tags):
         article_data["tags"].append(fake.word())
     return article_data
-
-
-# article_data = generate_random_article()
-# title_article = article_data["title"]
-# description_article = article_data["description"]
-# body_article = article_data["body"]
-# tags_article = article_data["tags"]
